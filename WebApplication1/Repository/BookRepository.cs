0857f42863a3527260125baa621c0c0b327cf7c3
cggtsd <cggtsds59@gmail.com>
2024-09-05 17:06:11 +0530

    first commit

---
+++ WebApplication1/Repository/BookRepository.cs
@@ -0,0 +1,62 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Repository
+{
+    public class BookRepository
+    {
+
+        public List<BookModelcs> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModelcs GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModelcs> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+
+        }
+
+        private List<BookModelcs> DataSource()
+        {
+
+            return new List<BookModelcs>()
+            {
+                new BookModelcs()
+                {
+                    Id = 1,
+                    Title = "MVC",
+                    Author = "Fatima"
+                },
+                new BookModelcs()
+                {
+                    Id = 2,
+                    Title = "Dot Net Core",
+                    Author = "Fatima"
+                },
+                new BookModelcs()
+                {
+                    Id = 3,
+                    Title = "C#",
+                    Author = "D.Fatima"
+                },
+                new BookModelcs()
+                {
+                    Id = 4,
+                    Title = "Java",
+                    Author = "Fariha"
+                },
+                new BookModelcs()
+                {
+                    Id = 5,
+                    Title = "Php",
+                    Author = "Lubna"
+                }
+
+            };
+        }
+    }
+}
---
+++ WebApplication1/TempView/MyView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.TempView
+{
+    public class MyViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
