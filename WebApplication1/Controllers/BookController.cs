88ab6fd6049df3d1b0025cdc7f18dc38312c0478
cggtsd <cggtsds59@gmail.com>
2024-09-06 17:10:19 +0530

    Dot Net Core First Commit

---
+++ WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+            
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult GetAllBooks()
+        {
+            //return "All books";
+            //return _bookRepository.GetAllBooks();
+            var books = _bookRepository.GetAllBooks();
+            return View();
+        }
+
+        public BookModelcs GetBook(int id)
+        {
+            //return $"book with id = {id}";
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModelcs> SearchBooks(string bookName,string authorName)
+        {
+            //return $"Book with name = {bookName} & Author = {authorName}";
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+    }
+}
---
+++ WebApplication1/Models/BookModelcs.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class BookModelcs
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
